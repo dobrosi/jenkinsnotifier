57c78db4a462c6d26a999bcc3860fb859c4c5446
andris <andris@freemail.hu>
2015-08-01 11:03:22 +0200

    1st

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # jenkinsnotifier
+
+Simple maven run: 
+mvn exec:java
+
+Create jar and execute standalone:
+mvn clean compile assembly:single
+java -classpath jenkins-notifier-0.1-jar-with-dependencies.jar hu.dobrosi.jenkinsnotifier.Main http://sample.url/job/ProjectName
+
+Configuration in pom.xml:
+jenkinsProjectUrl = Jenkins URL of your project what you want to follow.
---
+++ src/main/java/hu/dobrosi/jenkinsnotifier/Main.java
@@ -0,0 +1,75 @@
+package hu.dobrosi.jenkinsnotifier;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
+
+public class Main {
+	private static String url = "/rssAll";
+	private static int delay = 10000;
+	private static String jenkinsProjectUrl;
+
+	public static void main(String[] args) {
+		jenkinsProjectUrl = args[0];
+		new Main().start();
+	}
+
+	private Object ptitleText;
+
+	private void start() {
+		new Timer().schedule(new TimerTask() {
+
+			@Override
+			public void run() {
+				reload();
+			}
+		}, 0, delay);
+	}
+
+	private void reload() {
+		SAXBuilder builder = new SAXBuilder();
+
+		try {
+			InputStream in = new URL(jenkinsProjectUrl + url).openStream();
+			Document document = (Document) builder.build(in);
+			Element rootNode = document.getRootElement();
+			Namespace ns = rootNode.getNamespace();
+			Element feed = (Element) rootNode.getChildren("entry", ns).get(0);
+			Element title = feed.getChild("title", ns);
+			String titleText = title.getText();
+			System.out.println(titleText);
+			if (!titleText.equals(ptitleText)) {
+				String[] cmd;
+				if (isStable(titleText)) {
+					cmd = new String[] { "notify-send", "-t", "10000", titleText };
+				} else {
+					cmd = new String[] { "notify-send", "-u", "critical", titleText };
+				}
+
+				Runtime.getRuntime().exec(cmd);
+			}
+			ptitleText = titleText;
+		} catch (JDOMException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			System.err.println(
+					"URL not found: " + jenkinsProjectUrl + "\nPlease configure your project Jenknis URL in pom.xml (jenkinsProjectUrl), or add it as first execution parameter.");
+			System.exit(-1);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private boolean isStable(String titleText) {
+		return titleText.contains("(back to normal)") || titleText.contains("(stable)");
+	}
+}
